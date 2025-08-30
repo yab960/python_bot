9f6dc8213c07513a7edf8923002c936614e2b931
yab960 <yabsrasherif01786@gmail.com>
2025-08-30 13:04:52 +0300

    Add files via upload

--- game.php
+++ game.php
@@ -7,13 +7,14 @@ require __DIR__ . '/vendor/autoload.php';
 class RandomNumberServer implements MessageComponentInterface {
     protected $clients;
     protected $loop;
+    $this->bingoCards = $this->generateBingoCards(50);
 
     public function __construct($loop) {
         $this->clients = new \SplObjectStorage;
         $this->loop = $loop;
         echo "WebSocket server started.\n";
 
-        $this->startGenerator();
+         $conn->send(json_encode($this->bingoCards));
     }
 
     public function onOpen(ConnectionInterface $conn) {
@@ -37,7 +38,7 @@ class RandomNumberServer implements MessageComponentInterface {
 
     private function startGenerator() {
         $this->loop->addPeriodicTimer(1, function () {
-            $random = rand(1, 1000);
+            $random = rand(1, 75);
             $msg = "[" . date('H:i:s') . "] Random: $random\n";
             echo $msg;
 
@@ -53,7 +54,7 @@ $loop = React\EventLoop\Factory::create();
 
 // Get port from environment or default 8080
 $port = getenv('PORT') ?: 8080;
-
+startGenerator
 $webSock = new React\Socket\SocketServer("0.0.0.0:$port", [], $loop);
 
 $server = new Ratchet\Server\IoServer(
@@ -67,3 +68,41 @@ $server = new Ratchet\Server\IoServer(
 );
 
 $server->run();
+
+private function generateBingoCards($numCards = 50) {
+    $cards = [];
+    
+    // Define the number ranges for each column
+    $bingoRanges = [
+        'B' => range(1, 15),
+        'I' => range(16, 30),
+        'N' => range(31, 45),
+        'G' => range(46, 60),
+        'O' => range(61, 75)
+    ];
+
+    // Generate each Bingo card
+    for ($i = 0; $i < $numCards; $i++) {
+        $card = generateSingleCard($bingoRanges);
+        $cards[] = $card;
+    }
+
+    return $cards;
+}
+
+private function generateSingleCard($bingoRanges) {
+    $card = [];
+    
+    // Generate the 5 columns for the Bingo card (B, I, N, G, O)
+    foreach ($bingoRanges as $column => $range) {
+        $numbers = array_splice($range, 0, 5); // Get 5 unique numbers from the range
+        shuffle($numbers);  // Shuffle the numbers for randomness
+        $card[$column] = $numbers;
+    }
+
+    // Set the middle space to "Free" (N column, 3rd row)
+    $card['N'][2] = 'Free';
+    
+    return $card;
+}
+
